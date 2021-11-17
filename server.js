02e2027290768014ee02264fc4984b82af298edd
Carter Comeau <49622587+comeaucar@users.noreply.github.com>
2021-11-17 15:37:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const DB_URL = "mongodb+srv://cartercomeau:ENV['password']@comp3123.qdcbr.mongodb.net/101253879_assignment2?retryWrites=true&w=majority"
+const DB_URL = "mongodb+srv://cartercomeau:process.env.password@comp3123.qdcbr.mongodb.net/101253879_assignment2?retryWrites=true&w=majority"
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
