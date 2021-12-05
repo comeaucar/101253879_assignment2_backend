590a3e9fd94b66835318dc6c68b9241b2aa26cd3
comeaucar <cartercomeau@icloud.com>
2021-12-05 18:30:12 -0500

    updated package.json

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose.connect(DB_URL, {
 app.use('/', employeeRoutes)
 
 app.use('/', adminRoutes)
-
-app.listen(8029, ()=> {
-    console.log('Server is listening on port 8029')
+const PORT = process.env.PORT || 8080
+app.listen(PORT, ()=> {
+    console.log('Server is listening on port ' + PORT)
 });
